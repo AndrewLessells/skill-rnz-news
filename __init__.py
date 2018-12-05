1625f58ebf89164cfeb280af3abe61cc6c7d2171
AndrewLessells <45384671+AndrewLessells@users.noreply.github.com>
2018-12-05 16:11:43 +1300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -51,7 +51,23 @@ class NewsSkill(CommonPlaySkill):
     def url_rss(self):
         url_rss = final_bulletin_url
 
-        data = feedparser.parse(url_rss.strip())
+        feed_url = 'http://createfeed.fivefilters.org/extract.php?url=https%3A%2F%2Fwww.radionz.co.nz%2Fprogrammes%2Fnews-bulletin&in_id_or_class=o-digest+o-digest--audio+o-digest--extended+u-blocklink+no-thumbnail&url_contains=%2Fnews-bulletin%2F'
+
+        # Parsing the specified feed
+        rss_feed = feedparser.parse(feed_url)
+
+        # Setting the first feed entry as the latest bulletin
+        latest_bulletin = feed.entries[0].link
+
+        # Extracting the date/bulletin information from the url
+        try:
+            bulletin_extract = re.search('story/(.+?)/radio-new-zealand-news', latest_bulletin).group(1)
+        except AttributeError:
+            # AAA, ZZZ not found in the original string
+            bulletin_extract = '' # apply your error handling
+
+        final_bulletin_url = "https://www.radionz.co.nz/audio/player?audio_id={}".format(bulletin_extract)
+
         # After the intro, find and start the news stream
         # select the first link to an audio file
         for link in data['entries'][0]['links']:
